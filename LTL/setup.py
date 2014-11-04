cf5cf1c8e7f8105d7476fad47086d1a53e9ad0f9
Brian Luft <brian@lincolnloop.com>
2014-11-04 06:24:21 -0800

    Adds setup.py

---
+++ LTL/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='LightningTalksLive',
+      version='1.0',
+      description='Lightning Talks Live',
+      author='Lincoln Loop',
+      author_email='info@lincolnloop.com',
+      url='https://github.com/LincolnLoop/LightningTalksLive',
+      packages=['LTL'],
+     )
